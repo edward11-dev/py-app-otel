abe9415c0565c08a45b673786e9a6d91ed204589
Eddie <eahanks11@gmail.com>
2025-07-24 17:05:31 -0400

    app import updates

--- my-app.py
+++ my-app.py
@@ -1,7 +1,9 @@
+from opentelemetry import trace
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
 from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry import metrics as metric
 from opentelemetry.sdk.metrics import MeterProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
@@ -14,7 +16,7 @@ trace.set_tracer_provider(
     )
 )
 tracer = trace.get_tracer(__name__)
-span_processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="http://otel-collector:4317", insecure=True))
+span_processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="http://jaeger-collector.observability.svc.cluster.local:4317", insecure=True))
 trace.get_tracer_provider().add_span_processor(span_processor)
 
 # Set up metrics
@@ -22,7 +24,7 @@ metric.set_meter_provider(
     MeterProvider(
         metric_readers=[
             PeriodicExportingMetricReader(
-                OTLPMetricExporter(endpoint="http://otel-collector:4317", insecure=True)
+                OTLPMetricExporter(endpoint="http://jaeger-collector.observability.svc.cluster.local:4317", insecure=True)
             )
         ]
     )
