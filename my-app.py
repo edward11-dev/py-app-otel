3ea7751f8ce719894dff85dd18317c08131764da
edward11-dev <98662209+edward11-dev@users.noreply.github.com>
2025-07-24 14:38:55 -0400

    Create my-app.py
    seed app with otel sdks and basic otel export endpoint

---
+++ my-app.py
@@ -0,0 +1,32 @@
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+
+# Set up traces
+trace.set_tracer_provider(
+    TracerProvider(
+        resource=Resource.create({SERVICE_NAME: "my-python-app"})
+    )
+)
+tracer = trace.get_tracer(__name__)
+span_processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="http://otel-collector:4317", insecure=True))
+trace.get_tracer_provider().add_span_processor(span_processor)
+
+# Set up metrics
+metric.set_meter_provider(
+    MeterProvider(
+        metric_readers=[
+            PeriodicExportingMetricReader(
+                OTLPMetricExporter(endpoint="http://otel-collector:4317", insecure=True)
+            )
+        ]
+    )
+)
+
+# Auto-instrument HTTP requests
+RequestsInstrumentor().instrument()
